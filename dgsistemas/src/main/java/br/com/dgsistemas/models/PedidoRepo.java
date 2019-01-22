31b30703c340eaef39157bf8026b84f024307e43
danielmurilo <danielmurilo@yahoo.com.br>
2019-01-21 21:44:28 -0300

    make it better2

--- dgsistemas/src/main/java/br/com/dgsistemas/models/PedidoRepo.java
+++ dgsistemas/src/main/java/br/com/dgsistemas/models/PedidoRepo.java
@@ -16,11 +16,11 @@ public interface PedidoRepo extends CrudRepository<Pedido, Long> {
 			  nativeQuery = true)
 	public List<Pedido> listarPedidosPorConta(@Param("id") Long id);
 	
-	@Query(value = "SELECT IFNULL(SUM(p.valor_venda),0.00) AS 'totalDinheiro' FROM pedido p WHERE p.produto_id = 1 AND DATE(p.data) = :date", 
+	@Query(value = "SELECT COALESCE(SUM(p.valor_venda),0.00) AS  totalDinheiro  FROM pedido p WHERE p.produto_id = 1 AND DATE(p.data) = :date", 
 			  nativeQuery = true)
 	public Double valorTotalVendasEmDinheiroPorData(@Param("date") Date date);
 	
-	@Query(value = "SELECT IFNULL(SUM(p.valor_venda),0.00) AS 'totalCartao' FROM pedido p WHERE p.produto_id = 2 AND DATE(p.data) = :date", 
+	@Query(value = "SELECT COALESCE(SUM(p.valor_venda),0.00) AS  totalCartao  FROM pedido p WHERE p.produto_id = 2 AND DATE(p.data) = :date", 
 			  nativeQuery = true)
 	public Double valorTotalVendasEmCartaoPorData(@Param("date") Date date);
 	
